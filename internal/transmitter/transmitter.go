da61354eb261d5b55015c594b87460d27c3c54b3
masdjab <masdjab@gmail.com>
2024-05-22 16:22:28 +0700

    Sleep longer when no job

--- internal/transmitter/transmitter.go
+++ internal/transmitter/transmitter.go
@@ -2,6 +2,7 @@ package transmitter
 
 import (
 	"fmt"
+	"time"
 	log "github.com/sirupsen/logrus"
 	"cems-dis/model"
 )
@@ -23,9 +24,20 @@ func Init(model *model.Model) {
 }
 
 func Start() {
+	setResult := func (result Result) {
+		if result.IsSuccess() {
+			trx.model.SetTransmissionSuccess(result.Task(), result.Code(), result.Note())
+			log.Printf("transmitter.Start => %s", result.Info())
+		} else {
+			trx.model.SetTransmissionError(result.Task(), result.Code(), result.Note())
+			log.Warningf("transmitter.Start => %s", result.Info())
+		}
+	}
+
 	trx.stop = false
 
 	log.Println("Transmitter started")
+	sleepSecs(5)
 
 	for {
 		if trx.stop {
@@ -33,46 +45,38 @@ func Start() {
 			break
 		}
 
-		sleepSecs(1)
 		var tasks []*model.Transmission
 		err := trx.model.DB.Model(model.Transmission{}).Where("status = ?", "Pending").Order("updated_at").Limit(1).Find(&tasks).Error
 		if err != nil {
 			setResult(Error(nil, 0, fmt.Sprintf("Failed fetching transmission data: %s", err.Error())))
 		}
-		if len(tasks) > 0 {
-			var protocol Protocol
-			task := tasks[0]
-			if task.Protocol == "DUMMY" {
-				protocol = NewDummyProtocol(trx.model)
-			} else if task.Protocol == "PING" {
-				protocol = NewPingProtocol(trx.model)
-			} else if task.Protocol == "CEMS-MPN" {
-				protocol = NewCemsMpnProtocol(trx.model)
-			// } else if task.Protocol == "CEMS-KLHK" {
-			// 	protocol = NewKlhkProtocol(trx.model)
-			}
-
-			if protocol == nil {
-				setResult(Error(task, 0, "Unsupported protocol"))
-				continue
-			}
-
-			trx.model.SetTransmissionStarted(task)
-			setResult(protocol.Send(task))
+		if len(tasks) == 0 {
+			sleepSecs(1)
+			continue
 		}
+
+		var protocol Protocol
+		task := tasks[0]
+		if task.Protocol == "DUMMY" {
+			protocol = NewDummyProtocol(trx.model)
+		} else if task.Protocol == "PING" {
+			protocol = NewPingProtocol(trx.model)
+		} else if task.Protocol == "CEMS-MPN" {
+			protocol = NewCemsMpnProtocol(trx.model)
+		// } else if task.Protocol == "CEMS-KLHK" {
+		// 	protocol = NewKlhkProtocol(trx.model)
+		}
+
+		if protocol == nil {
+			setResult(Error(task, 0, "Unsupported protocol"))
+			continue
+		}
+
+		trx.model.SetTransmissionStarted(task)
+		setResult(protocol.Send(task))
 	}
 }
 
 func Stop() {
 	trx.stop = true
-}
-
-func setResult(result Result) {
-	if result.IsSuccess() {
-		trx.model.SetTransmissionSuccess(result.Task(), result.Code(), result.Note())
-		log.Printf("transmitter.Start => %s", result.Info())
-	} else {
-		trx.model.SetTransmissionError(result.Task(), result.Code(), result.Note())
-		log.Warningf("transmitter.Start => %s", result.Info())
-	}
 }
